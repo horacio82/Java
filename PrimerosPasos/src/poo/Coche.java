34b3ba2c52edc61a382da3559acb87fd731fb919
horacio <horacio@horacio-GA-A55M-DS2>
2020-12-20 00:52:45 -0300

    Version 1

---
+++ PrimerosPasos/src/poo/Coche.java
@@ -0,0 +1,20 @@
+package poo;
+//Curso Java. POO II. Vídeo 28
+	//https://youtu.be/ZY5pwm92cWQ
+
+public class Coche {
+
+	int ruedas;
+	int largo;
+	int ancho;
+	int motor;
+	int peso;
+	
+	public Coche() {
+		ruedas = 4;
+		ancho = 300;
+		motor = 1600;
+		peso = 500;
+	}
+	
+}
