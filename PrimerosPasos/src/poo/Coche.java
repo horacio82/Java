c538f52aaec2df8a66eda3dcb487bd83840623d3
Horacio Molinari <molinarih2011@gmail.com>
2021-02-06 11:58:24 -0300

    Modificado

--- PrimerosPasos/src/poo/Coche.java
+++ PrimerosPasos/src/poo/Coche.java
@@ -1,20 +1,38 @@
 package poo;
-//Curso Java. POO II. Vídeo 28
+//Curso Java. POO II. Vídeo 28, 29, 30
 	//https://youtu.be/ZY5pwm92cWQ
 
 public class Coche {
-
-	int ruedas;
-	int largo;
-	int ancho;
-	int motor;
-	int peso;
+	//Encapsulación de datos:
+	private int ruedas;
+	private int largo;
+	private int ancho;
+	private int motor;
+	private int peso_plataforma;
+	String color;
+	int peso_total;
+	boolean asientos_cuero, climatizador;
 	
 	public Coche() {
+		//Método constructor (Propiedad = valor;)
 		ruedas = 4;
+		largo = 2000;
 		ancho = 300;
 		motor = 1600;
-		peso = 500;
+		peso_plataforma = 500;
 	}
 	
+	//Métodos getters:
+	public String dime_largo() {
+		return "El largo del coche es: "+largo;
+	}
+	
+	//Métodos setters:
+	public void establece_color() {
+		color = "Azul";
+	}
+	
+	public String dime_color() {//Getter
+		return "El color del coche es: "+color;
+	}
 }
--- PrimerosPasos/src/poo/Uso_Coche.java
+++ PrimerosPasos/src/poo/Uso_Coche.java
@@ -1,13 +1,18 @@
 package poo;
 
-public class Uso_Coche {
+public class Uso_Coche {//Esta es la clase principal.
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Coche Renault = new Coche(); //INTANCIAR UNA CLASE. EJEMPLAR DE CLASE.
 		
-		System.out.println("Este coche tiene "+Renault.ruedas+" ruedas");
+		Coche micoche=new Coche();
+		micoche.establece_color();
+		System.out.println(micoche.dime_color());
+
+		//Coche Renault = new Coche(); //INTANCIAR UNA CLASE. EJEMPLAR DE CLASE.
+		
+		//System.out.println(Renault.dime_largo());
+		
+		
 	}
 
 }
