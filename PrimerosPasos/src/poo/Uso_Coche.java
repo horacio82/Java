e7710ff24fbee98527c84c19f20f7a1defec0e53
Horacio Molinari <molinarih2011@gmail.com>
2021-04-11 11:48:36 -0300

    Modificado

--- PrimerosPasos/src/poo/Uso_Coche.java
+++ PrimerosPasos/src/poo/Uso_Coche.java
@@ -1,16 +1,22 @@
 package poo;
+import javax.swing.*;
 
 public class Uso_Coche {//Esta es la clase principal.
 
 	public static void main(String[] args) {
 		
-		Coche micoche=new Coche();
-		micoche.establece_color("verde.");
+		Coche micoche=new Coche();//Instancia
+		micoche.establece_color(JOptionPane.showInputDialog("Introduce un color: "));
 		System.out.println(micoche.dime_datos_generales());
 		System.out.println(micoche.dime_color());
 		
-		micoche.configura_asientos("si");
+		micoche.configura_asientos(JOptionPane.showInputDialog("¿Tiene asientos de cuero?: "));
 		System.out.println(micoche.dime_asientos());
+		
+		micoche.configura_climatizador(JOptionPane.showInputDialog("¿Tiene climatizador?: "));
+		System.out.println(micoche.dime_climatizador());
+		System.out.println(micoche.dime_peso_coche());
+		System.out.println("El precio del coche es " + micoche.precio_coche());
 
 		//Coche Renault = new Coche(); //INTANCIAR UNA CLASE. EJEMPLAR DE CLASE.
 		
